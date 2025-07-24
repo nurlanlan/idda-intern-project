959922edd143e8e9cb76bdbc7a96f0ba2f84ccd4
nurlanlan <nurahmad004@gmail.com>
2025-07-24 12:30:47 +0400

    card entity created

---
+++ card-service/src/main/java/entity/Card.java
@@ -0,0 +1,39 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Data
+@Table(name = "cards")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String encryptedCardNumber;
+
+    @Column(nullable = false)
+    private String encryptedCvv;
+
+    @Column(nullable = false)
+    private float balance;
+
+    @Column(nullable = false)
+    private boolean isPrimary;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private String expirationDate;
+
+
+}

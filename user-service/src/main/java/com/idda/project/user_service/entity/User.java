2c304d5550289c2284b20aecb7e0f2d46d9a9288
nurlanlan <nurahmad004@gmail.com>
2025-07-25 15:42:49 +0400

    User Details updated

--- user-service/src/main/java/com/idda/project/user_service/entity/User.java
+++ user-service/src/main/java/com/idda/project/user_service/entity/User.java
@@ -4,12 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
 
 

bf4b52c133df558e0550c24b210e72827cf6c39d
nurlanlan <nurahmad004@gmail.com>
2025-07-29 10:04:43 +0400

    rabbit mq init

---
+++ user-service/src/main/java/com/idda/project/user_service/config/RabbitMQConfig.java
@@ -0,0 +1,39 @@
+package com.idda.project.user_service.config;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String EXCHANGE_NAME = "auth.user.exchange";
+    public static final String QUEUE_NAME = "user.registration.queue";
+    public static final String ROUTING_KEY = "user.registered";
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
